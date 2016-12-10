bc33eac64d35132c3aab9df52b16c926de60aa4e
Kirill Romanov <djaler1@gmail.com>
2016-12-10 15:12:47 +0300

    Правка ошибки

--- controller/__init__.py
+++ controller/__init__.py
@@ -2,7 +2,5 @@ from controller.base_controller import BaseController
 from controller.create_keys_controller import CreateKeysController
 from controller.export_public_controller import ExportPublicController
 from controller.import_public_controller import ImportPublicController
-from controller.main_controller import MainController
 from controller.send_controller import SendController
-
-
+from controller.main_controller import MainController
